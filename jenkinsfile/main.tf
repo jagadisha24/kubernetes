8dbfbf4533cbd3c87c6a95a5e028981b2d340ccc
jagdish <jagdishsiddaiah2408@gmail.com>
2024-12-02 14:05:08 +0530

    changed e2

---
+++ jenkinsfile/main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+
+region = "ap-south-1"
+
+aws access_key = "  "
+
+aws secret_access_key = "  "
+
+resources = "aws_instances"
+
+AMI_iD = " "
+seurity_group "default"
+
+storage = "8gb"
+}
